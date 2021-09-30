e6c774ad1d9722184481d1737408c47db7b1758a
julioGN <navaguerrerojj@appstate.edu>
2021-09-30 01:22:33 -0400

    done i think

--- eddysmethod.py
+++ eddysmethod.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-S1 = 'ACGU'
-S2 = 'CGAU'
+S1 = 'TTCATA'
+S2 = 'TGCTCGTA'
 s_s1 = []
 s_s2 = []
 diagonal = '↖'
@@ -12,37 +12,34 @@ stop = 'S'
 
 def print_eddy(seq1, seq2):
     m = np.zeros((len(S1) + 1, len(S2) + 1))
-    bt = [[stop for a in range(len(S1) + 1)] for b in range(len(S2) + 1)]
-
+    bt = [[stop for a in range(len(S2) + 1)] for b in range(len(S1) + 1)]
     i = 0
     for matching in m:
         j = 0
         for cells in matching:
+            m[0][j] = j * (-6)
+            m[i][0] = i * (-6)
             if i != 0 and j != 0:
                 if S1[i-1] == S2[j-1]:
-                    m[i][j] = m[i-1][j-1] + 1
-                    # m[i][j] = m[i-1][j-1] + 5
+                    m[i][j] = m[i-1][j-1] + 5
                     bt[i][j] = diagonal
                 if S1[i-1] != S2[j-1]:
-                    m[i][j] = m[i - 1][j - 1]
-                    # m[i][j] = m[i-1][j-1] - 2
+                    m[i][j] = m[i-1][j-1] - 2
                     bt[i][j] = diagonal
-                    if m[i-1][j] > m[i][j]:
-                        m[i][j] = m[i-1][j]
-                        # m[i][j] = m[i - 1][j] - 6
-                        bt[i][j] = up
-                    if m[i][j-1] > m[i][j]:
-                        m[i][j] = m[i][j - 1]
-                        # m[i][j] = m[i][j-1] - 6
-                        bt[i][j] = left
+                if (m[i-1][j] - 6) > m[i][j]:
+                    m[i][j] = m[i - 1][j] - 6
+                    bt[i][j] = up
+                if (m[i][j-1] - 6) > m[i][j]:
+                    m[i][j] = m[i][j-1] - 6
+                    bt[i][j] = left
             j += 1
         i += 1
 
     print("S1 = " + S1 + ", S2 = " + S2)
-    print("M: ")
+    print("Eddy's method: ")
     print(np.matrix(m))
     max_align_score = m[i-1][j-1]
-    print("Maximum alignment score: ", end='')
+    print("Optimal score: ", end='')
     print(max_align_score)
     print()
     print("Backtracking Matrix: ")
@@ -77,6 +74,7 @@ def print_eddy(seq1, seq2):
     print("S1: " + aligned_s1)
     print("S2: " + aligned_s2)
 
+    #print("Optimal score: ", end='')
 
 def main():
     print_eddy(S1, S2)
